47664df5cbf12f471f66beaf11366107055c8ec5
Lisa Mishra <lisamishra@Lisas-MacBook-Pro.local>
2017-08-14 21:43:14 -0500

    add new home

---
+++ app/data/friends.js
@@ -0,0 +1,15 @@
+//test data
+var friends = [
+    {
+        "name":"Jim",
+        "photo":"https://az616578.vo.msecnd.net/files/2016/03/04/635927223188301534-622930799_JIM.png",
+        "scores":[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    },
+    {
+        "name":"Dwight",
+        "photo":"https://upload.wikimedia.org/wikipedia/en/c/cd/Dwight_Schrute.jpg",
+        "scores":[5, 2, 4, 4, 5, 1, 2, 5, 4, 1]
+    },
+];
+
+module.exports = friends; 
